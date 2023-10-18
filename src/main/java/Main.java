08fed3d13713cf78f3139cb19abd2dfa117f5230
artyomkorolev <Artyom.King.03@gmail.com>
2023-10-18 13:02:45 +0300

    Sprint_02

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,30 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("На скольких человек необходимо разделить счёт?");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Некорректный ввод количества человек. Повторите ввод:");
+            scanner.next();
+        }
         int countPersons = scanner.nextInt();
+
         while(true){
             if(countPersons<1){
                 System.out.println("Некорректное значение");
                 System.out.println("На скольких человек необходимо разделить счёт?");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Некорректный ввод количества человек. Повторите ввод:");
+                    scanner.next();
+                }
                 countPersons = scanner.nextInt();
             }else if(countPersons==1){
                 System.out.println("Нет смысла считать!");
                 System.out.println("На скольких человек необходимо разделить счёт?");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Некорректный ввод количества человек. Повторите ввод:");
+                    scanner.next();
+                }
                 countPersons = scanner.nextInt();
-            }else if(countPersons>1){
+            }else {
 
                 break;
             }
